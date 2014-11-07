aa22b283e5deac3efab038056774a9cfa476b6e3
Kenneth Dahle <Kenneth.dahle.1996@gmail.com>
2014-11-07 02:43:50 +0100

    updated module-date

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@
 
 module_name = "BONES-MODULE-KD_ESSENTIALS"
 module_version = "v0.0.2-DEV"
-module_date = "18. Aug 2014 02:41 CEST"
+module_date = "07. Nov 2014 02:43 CET"
 
 import os, sys
 import string, random
